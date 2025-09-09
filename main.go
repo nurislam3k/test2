a5ce392afbcf91499ba6ef4dde19d9c8051cf98f
nurislam3k <nurislamulanbekovich@gmail.com>
2025-09-09 20:24:52 +0500

    Aruzham

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"test/test"
+	"test/test/nurislam"
+)
 
 func main() {
 	fmt.Println("dlfkdkfldkf")
+	test.Add()
+	nurislam.Nurislam() 
+	arip := nurislam.Aruzhan(9)
+	if arip == 80 {
+		fmt.Println("correct answer")
+	} else {
+		fmt.Println("wrong answer")
+	}
+	fmt.Println(arip)
 }
+
---
+++ test/nurislam/aryxzha.go
@@ -0,0 +1,10 @@
+package nurislam
+func Nurislam() {
+	
+}
+
+func Aruzhan(a int) int {
+	san := 10
+	result := san * a
+	return result
+}
